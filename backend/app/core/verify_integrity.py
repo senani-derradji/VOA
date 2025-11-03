b175000de5e87a5d3027c9feb0ede6bbed6316ef
senani-derradji <derradji.senani.1@gmail.com>
2025-11-03 03:32:24 +0100

    Fix Verify_integrity Issue (Logic)

--- backend/app/core/verify_integrity.py
+++ backend/app/core/verify_integrity.py
@@ -1,19 +1,34 @@
-from app.models.audit import AuditModel as AUD
 from sqlalchemy.orm import Session
-from app.utils.logging_logs import get_logger
+import os,sys
 from hashlib import sha256
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+
+from app.models.audit import AuditModel as AUD
+from app.utils.logging_logs import get_logger
+
 
 logger = get_logger(__name__)
 
 def verify(db: Session):
     logs = db.query(AUD).order_by(AUD.id).all()
-    previous_hash = "0"
-    for log in logs:
-        data = f"{str(previous_hash)}:{log.user_id}:{log.action}"
-        check = sha256(data.encode()).hexdigest()
-        if check != log.integrity_checks:
-            logger.warning(f"Integrity broken at log ID {log.id}")
+    if len(logs) <= 2:
+        logger.warning("No logs <= 2")
+        return False
+
+    for i in range(len(logs) - 1):
+        try:
+            log = logs[i]
+            next_log = logs[i + 1]
+            previous_hash = log.integrity_checks if log.integrity_checks else "0"
+            data = f"{str(previous_hash)}:{str(log.user_id)}:{log.action}"
+            check = sha256(data.encode()).hexdigest()
+            if check != next_log.integrity_checks:
+                logger.warning(f"Integrity broken at log ID {log.id}")
+                return False
+        except Exception as e:
+            logger.error(f"Integrity check failed: {e}")
             return False
-        previous_hash = check
+
     logger.info("Integrity check passed")
     return True
